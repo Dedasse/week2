55c88afb65e2ce6643673f8910dbb4c76f5c1c65
mikaely <mikael.ylivaara@metropolia.fi>
2020-03-29 23:29:33 +0300

    s

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,16 +7,30 @@ const user_list_get = (req, res) => {
   res.json(users);
 };
 
-const user_get = (req, res) => {
+const user_get = async(req, res) => {
   console.log('user id parameter', req.params);
-  const user = users.filter(user => user.id === req.params.id).pop();
+  
+  const user = await userModel.getUser(req.params.id);
   delete user.password;
   res.json(user);
 };
 
-const user_post = (req, res) => {
-  console.log('data from form', req.body);
-  res.send('With this endpoint you can add users');
+const user_post = async(req, res) => {
+  console.log('data from form', req.body, );
+  const inUser = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  try {
+    const user = await userModel.addUser(inUser);
+    console.log('inserted', user);
+    res.send(`added user: ${user}`);
+  } catch (e) {
+    console.error('problem with cat_post in catController', e);
+    res.status(500).send(`database insert error: ${e.message}`);
+  }
+  
 };
 
 module.exports = {
--- models/userModel.js
+++ models/userModel.js
@@ -14,16 +14,24 @@ const users = [
   },
 ];
 
-const getUsersList = () => {
-  // remember export
-  // SELECT user_id, name, email FROM wop_user
+const promisePool = require('../database/db').promise();
+const getUsersList = async() => {
+  try {
+    const [rows] = await promisePool.query('SELECT user_id, name, email FROM wop_user');
+    return rows;
+  } catch (e) {
+    console.error('error', e.message);
+  }
+
 };
 
-const getUser = (id) => {
-  // SELECT * FROM wop_user WHERE user_id = ?
-  // remember in controller do :
-  // delete user.password;
-  // before sending the user back
+const getUser = async(id) => {
+  try{
+    const [rows]= await promisePool.query('SELECT * FROM wop_user WHERE user_id=?', [id]);
+    return rows[0];
+  }catch (e){
+    console.error('error', e.message);
+  }
   const user = users.filter((usr) => {
     if (usr.user_id === id) {
       return usr;
@@ -41,9 +49,21 @@ const getUserLogin = (email) => {
   return user[0];
 };
 
+const addUser= async(user)=>{
+  try {
+    console.log('insert user?', user);
+    const [rows] = await promisePool.query('INSERT INTO wop_user( name, email, password ) VALUES (?,?,?)', [ user.name, user.email, user.password ]);
+    return rows;
+  } catch (e) {
+    console.error('updateUser model crash', e.message);
+  }
+};
+
 module.exports = {
   users,
+  getUsersList,
   getUser,
   getUserLogin,
+  addUser,
 };
 
